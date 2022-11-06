c46763f512315855cd4a39309e12f056419024d0
Leo Martínez <49732181+Leo-Mtz@users.noreply.github.com>
2022-11-05 20:57:51 -0600

    Update admin.h

--- admin.h
+++ admin.h
@@ -1,6 +1,7 @@
 void menuAdmin(){
 
 	int opc = 0;
+	int op1=0;
 	
 	do{
 		
@@ -11,6 +12,7 @@ void menuAdmin(){
 		printf("4.- Modificar Rutas\n");		
 		printf("5.- Mostrar Usuarios\n");
 		printf("6.- Mostrar Rutas\n");	
+		printf("7.- Consultar registros de pasajeros del programa\n");
 		opc = validaEntero("Que desea realizar? ");
 		
 	
@@ -79,19 +81,160 @@ void menuAdmin(){
 					break;
 					
 			case 7:
-			
-					menuPasajero();
-					
+							
+					menuConsultas();		
 					break;			
 										
 		}
 		
-	} while(opc<5);
+	} while(opc<7);
 	
+}
+
+void menuConsultas(){
+	
+		do{
+		
+		system("cls");
+		printf("Cómo desea consultar los registros de pasajeros");
+		printf("1.- ID de pasajero\n");
+		printf("2.- Rutas menos solicitadas\n");
+		printf("3.- Rutas más socilitadas\n");		
+		printf("4.- Pasajeros que tomaron el autobus\n");
+		printf("5.- Pasajeros que no tomaron el autobus\n");	
+		printf("6.- Pasajeros que se bajaron del autobus\n");
+		printf("7.- Regresar al menu principal");
+		op1 = validaEntero("Que desea realizar? ");
+		
+		
+		
+	
+		switch(op1)
+		{
+			case 1:
+					
+					printf("ID");
+					break;
+								
+			case 2:
+			
+			
+						
+			case 3:
+			
+					//modificarUsuario(); cambiar a inactivo, no ingresar estado de ruta o usuario
+					
+					break;
+			
+			case 4: 
+			
+					modificarRuta();			
+					
+					break;
+					
+			case 5:
+				
+					lecturaListaUsuario();
+					mostrarListas();
+					
+					break;
+			
+			case 6: 
+			
+					archivoLecturaRuta();
+					mostrarRuta();			
+					
+					break;
+					
+			case 7:
+							
+					menuAdmin();		
+					break;			
+										
+		
+		}
+		} while(op1<7);	
 }
 
 
 
+
+void consultaID(Pasajero *aux){
+	
+	Factura *aux = primero;
+	Factura *actual, *previo, *otro;
+	actual=primero;
+	previo=actual;
+	int id=0, op;
+	bool bandera;
+	             
+	
+	id=validaEntero("Num de Factura a consultar: ");
+
+	while(actual!=NULL){
+		if(id==actual->idfactura){
+			//system("cls");1
+			bandera=true;
+		
+				 		 	
+			cout<<"Pasajero consultado\n"<<endl;
+			cout<<"idPasajero"<<setw(10);
+			cout<<"Ruta de autobus tomada"<<setw(10);
+			cout<<"Estacion desde donde toma el autobus"<<setw(10);
+			cout<<"Baja del autobus(1=si/2=no): "<<setw(10);
+			cout<<""<<setw(10);
+			cout<<"CP de receptor"<<setw(10);
+			cout<<"RFC de receptor"<<setw(10);
+			printf(" \t");
+			cout<<endl;
+			
+			cout<<endl;
+			
+			cout<<actual->idfactura<<setw(10);
+			cout<<actual->emisor<<setw(10);
+			cout<<actual->cpemisor<<setw(10);
+			cout<<actual->RFCemisor<<setw(10);
+			cout<<aux->receptor<<setw(10);
+			cout<<aux->cpreceptor<<setw(10);
+			cout<<aux->RFCreceptor<<setw(10);
+			
+			cout<<endl;
+			break;
+		}
+
+		else{
+			previo=actual;
+			actual=previo->sig;
+			actual->ant = previo;
+		}
+	
+	if(bandera==false){
+		cout<<"El numero de factura no existe"<<endl;
+	}
+	getch();
+
+}
+}
+
+void consultarxmenossolicitada()
+{
+	
+}
+
+void consultarxmas(){
+}
+
+void pasajerostomaron(){
+	
+}
+
+void pasajerosnotomaron(){
+	
+}
+
+void pasajerosbajaron(){
+	
+}
 
 
 
